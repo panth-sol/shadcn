ee9d1f9f8d63ee1ddcd786c67e7a497da8b663ad
panth-sol <panth.solanki@allen.in>
2023-09-10 17:17:26 +0530

    shadcnPreset added

--- tailwind.config.ts
+++ tailwind.config.ts
@@ -1,11 +1,9 @@
 import type { Config } from "tailwindcss"
-import animatePlugin from "tailwindcss-animate"
-import { shadcnPlugin } from "./lib/shadcn-plugin"
+import { shadcnPreset } from "./lib/shadcn-preset"
 
 const config = {
-  darkMode: ["class"],
+  presets: [shadcnPreset],
   content: ["app/**/*.{ts,tsx}", "components/**/*.{ts,tsx}"],
-  plugins: [animatePlugin, shadcnPlugin],
 } satisfies Config
 
 export default config
